7753037764473d4a01ab3430dc22823b53fd4271
sjp761 <171447425+sjp761@users.noreply.github.com>
2026-03-17 23:21:15 -0500

    Refresh UI after game is done downloading or located

--- maxima-ui/src/bridge_processor.rs
+++ maxima-ui/src/bridge_processor.rs
@@ -75,6 +75,10 @@ pub fn frontend_processor(app: &mut MaximaEguiApp, ctx: &egui::Context) {
                         }
                         app.installer_state.locate_response = Some(res);
                         app.installer_state.locating = false;
+                        app.backend
+                            .backend_commander
+                            .send(bridge_thread::MaximaLibRequest::GetGamesRequest)
+                            .unwrap();
                     }
                     DownloadProgressChanged(offer_id, progress) => {
                         if let Some(dl_ing) = app.installing_now.as_mut() {
@@ -84,7 +88,12 @@ pub fn frontend_processor(app: &mut MaximaEguiApp, ctx: &egui::Context) {
                             }
                         }
                     }
-                    DownloadFinished(_) => {}
+                    DownloadFinished(_) => {
+                        app.backend
+                            .backend_commander
+                            .send(bridge_thread::MaximaLibRequest::GetGamesRequest)
+                            .unwrap();
+                    }
                     DownloadQueueUpdate(current, queue) => {
                         if let Some(current) = current {
                             if !app.installing_now.as_ref().is_some_and(|n| n.offer == current) {
